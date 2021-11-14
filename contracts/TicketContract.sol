5dd1bc47ea64b5c896f773fe2490d9ae6e928687
@javierlinked <javierlinked@gmail.com>
2021-11-14 14:42:01 +0100

    fixes in ut

--- contracts/TicketContract.sol
+++ contracts/TicketContract.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 
 /// @custom:security-contact @javierlinked
-contract TicketContract is ERC1155, Ownable, ERC1155Burnable {
+contract TicketContract is ERC1155, Ownable, ERC1155Pausable, ERC1155Burnable {
     uint256 public constant CINEMA = 1;
     uint256 public constant FUTBOL = 2;
 
@@ -20,6 +21,8 @@ contract TicketContract is ERC1155, Ownable, ERC1155Burnable {
         _mint(msg.sender, CINEMA, 100, "");
         _mint(msg.sender, FUTBOL, 100, "");
     }
+
+    /// overrides only add 'onlyOwner' restriction
 
     // TODO: adapt to IPFS
     function setURI(string memory newuri) public onlyOwner {
@@ -35,6 +38,14 @@ contract TicketContract is ERC1155, Ownable, ERC1155Burnable {
         _mint(account, id, amount, data);
     }
 
+    function burn(
+        address account,
+        uint256 id,
+        uint256 amount
+    ) public override onlyOwner {
+        _burn(account, id, amount);
+    }
+
     function mintBatch(
         address to,
         uint256[] memory ids,
@@ -42,5 +53,42 @@ contract TicketContract is ERC1155, Ownable, ERC1155Burnable {
         bytes memory data
     ) public onlyOwner {
         _mintBatch(to, ids, amounts, data);
+    }
+
+    /**
+     * @dev Pauses all token transfers.
+     *
+     * See {ERC1155Pausable} and {Pausable-_pause}.
+     *
+     * Requirements:
+     *
+     * - the caller must be owner.
+     */
+    function pause() public virtual onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     *
+     * See {ERC1155Pausable} and {Pausable-_unpause}.
+     *
+     * Requirements:
+     *
+     * - the caller owner.
+     */
+    function unpause() public virtual onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override(ERC1155, ERC1155Pausable) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
--- test/ticketservice.test.js
+++ test/ticketservice.test.js
@@ -1,26 +1,77 @@
 const TicketContract = artifacts.require("TicketContract");
 
+// beforeEach(async function () {
+//   console.log("beforeEach");
+// });
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("TicketContract", async accounts => {
-  const [contractOwner, alice] = accounts;
+  let contractOwner, alice, bob;
+  let contract;
 
-  it("should assert true", async  () => {
-    const contract = await TicketContract.deployed();
+  beforeEach(async () => {
+    console.log("beforeEach");
+    [contractOwner, alice, bob] = accounts;
+    contract = await TicketContract.new();
+  });
+
+  // 1 Partner mints tickets
+
+  
+// 2 Partner transfers tickets
+// 3 Customer transfers tickets
+// 4 Presents tickets and X burns tickets
+
+  it("should assert true", async () => {
     console.log(await contract.contract.methods.CINEMA().call());
     return assert.isTrue(true);
   });
 
   it("should transfer", async () => {
-    const contract = await TicketContract.deployed();
+    const abountToTransfer = 2;
 
-    await contract.safeTransferFrom(contractOwner, alice, 1, 10, web3.utils.fromAscii('hola'));
+    // en un objecto se pasa el msg como contexto ?
+    await contract.safeTransferFrom(contractOwner, alice, 1, abountToTransfer, web3.utils.fromAscii('hola'), { from: contractOwner });
 
-    assert.equal(await contract.balanceOf(alice, 1), 10);
-    assert.equal(await contract.balanceOf(contractOwner, 1), 90);
-    
+    // error
+    // await contract.safeTransferFrom(contractOwner, alice, 1, 10, web3.utils.fromAscii('hola'), {from: alice});
+
+
+    assert.equal(await contract.balanceOf(alice, 1), abountToTransfer);
+    assert.equal(await contract.balanceOf(contractOwner, 1), 100 - abountToTransfer);
+
+  });
+
+  it("should transfer2", async () => {
+    const abountToTransfer = 2;
+
+    // en un objecto se pasa el msg como contexto ?
+    await contract.safeTransferFrom(contractOwner, alice, 1, abountToTransfer, web3.utils.fromAscii('hola'), { from: contractOwner });
+
+    // error
+    // await contract.safeTransferFrom(contractOwner, alice, 1, 10, web3.utils.fromAscii('hola'), {from: alice});
+
+
+    assert.equal(await contract.balanceOf(alice, 1), abountToTransfer);
+    assert.equal(await contract.balanceOf(contractOwner, 1), 100 - abountToTransfer);
+
+  });
+
+
+  it("should transfer3", async () => {
+    const abountToTransfer = 2;
+
+    // en un objecto se pasa el msg como contexto ?
+    await contract.safeTransferFrom(contractOwner, alice, 1, abountToTransfer, web3.utils.fromAscii('hola'), { from: contractOwner});
+    // error
+    // await contract.safeTransferFrom(contractOwner, alice, 1, 10, web3.utils.fromAscii('hola'), {from: alice});
+
+
+    assert.equal(await contract.balanceOf(alice, 1), abountToTransfer);
+    assert.equal(await contract.balanceOf(contractOwner, 1), 100 - abountToTransfer);
+
   });
 });
