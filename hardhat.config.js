2f1a58a5e8452bae8b3a6bb0372c72c2611f233e
@javierlinked <javierlinked@gmail.com>
2022-09-06 16:30:58 +0200

    add client-next

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,23 @@ require("@nomiclabs/hardhat-truffle5");
 const dotenv = require("dotenv");
 dotenv.config();
 const { PROJECT_ID, MNEMONIC, ETHERSCAN_API_KEY, PK } = process.env;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
+      inject: false,
       // accounts: {
       //   mnemonic: MNEMONIC
       // },
